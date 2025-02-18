e6e77b6d8fc7ff0203ec4dc4b757a2799ce85fbf
Anushriupadhye03 <102174586+Anushriupadhye03@users.noreply.github.com>
2025-02-18 22:47:33 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,158 @@
+/* class Task{
+ public static void main(String args[]){
+ 
+ System.out.print("Hello, I am Anushree Upadhye" );
+
+}
+} */
+
+/* class Addition{
+public static void main(String args[]){
+ 
+  int a = 74;
+  int b = 36;
+  int sum = a + b;
+  
+  System.out.println("a = " + a) ;
+
+  System.out.println("b = " + b);
+
+  System.out.println("sum = "+ sum); 
+}
+} */
+
+/* class Division{
+ public static void main(String args[]){
+ int num1 = 18;
+ int num2 = 5;
+ int div = num1/num2;
+
+ System.out.println("num1 = " + num1);
+ System.out.println("num2 = " + num2);
+ System.out.println("div = " + div);
+}
+} */
+
+/* class Operations{
+public static void main(String args[]){
+  int a = 5;
+  int b = 8;
+  int c = 6;
+  int d = 55;
+  int e = 9;
+  int f = 20;
+  int g = 3;
+  int h = 15;
+  int i = 2;
+
+  int ope1 = (-a + b * c);
+  int ope2 = (d + e) % 9;
+  int ope3 = f + (-g) * a/b;
+  int ope4 = a + h/g * i -b % g;
+
+  System.out.println(ope1);
+  System.out.println(ope2);
+  System.out.println(ope3);
+  System.out.println(ope4);
+}
+} */
+
+
+/* Question 5*/
+
+/*import java.util.Scanner;
+
+class Multiply{
+  public static void main(String args[]){
+  
+  Scanner scanner = new Scanner(System.in);
+
+  System.out.print("Enter 1st integer: ");
+  int num1 = scanner.nextInt();
+  System.out.print("Enter 2nd integer: ");
+  int num2 = scanner.nextInt();
+  System.out.print("Multiplication of above two numbers: " + num1 * num2);
+  
+  
+}
+} */
+
+/* Question6*/
+
+/* import java.util.Scanner;
+
+class Multiop{
+  public static void main(String args[]){
+  
+  Scanner scanner = new Scanner(System.in);
+
+  System.out.print("Enter 1st integer: ");
+  int num1 = scanner.nextInt();
+  System.out.print("Enter 2nd integer: ");
+  int num2 = scanner.nextInt();
+
+  System.out.println("Addition of above two numbers: " + (num1 + num2));
+  System.out.println("Substraction of above two numbers: " + (num1 - num2));
+  System.out.println("Multiplication of above two numbers: " + num1 * num2);
+  
+  
+}
+} */
+
+
+/*Question 7*/
+ 
+/* import java.util.Scanner;
+class Tables{
+  public static void main(String args[]){
+
+   Scanner scanner = new Scanner(System.in);
+
+   System.out.print("Enter an integer : ");
+   int num = scanner.nextInt();
+   
+   for (int i = 1; i <= 10; i++) {
+   System.out.println(num + " x " + i + " = " + (num * i));
+  }
+ scanner.close();
+}
+} */
+
+
+//Question 8
+
+/* import java.util.Scanner;
+
+ class Swapnum{
+ public static void main(String arga[]){
+
+ System.out.print("Type two numbers:");
+
+ Scanner scanner = new Scanner(System.in);
+
+ int num1 = scanner.nextInt();
+ int num2 = scanner.nextInt(); 
+
+ System.out.println("input numbers: " +" "+num1 +" "+ num2);
+ System.out.println("swapped numbers: " +" "+num2 +" "+ num1);
+  }
+ } */
+
+
+//Question 9
+
+/* class Cirarea{
+  public static void main(String args[]){
+   
+  int r = 5;
+  int area = 22/7 * r*r ;
+
+  System.out.print(" Area of circle is :" + " " + area);
+}
+} */
+
+
+//Question 10 & 11 are done in day2 assignment
+
+//Question 12
+
